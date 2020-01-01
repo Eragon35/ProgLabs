b33af8aa33f1212e95d80ad4a8014d072a0f49e6
Triada <antipin35@yahoo.com>
2020-01-01 20:13:08 +0300

    Refactor code: add some methods

--- src/Lab5/Console.java
+++ src/Lab5/Console.java
@@ -1,11 +1,8 @@
 package Lab5;
 
 import Lab3.*;
-
 import java.util.*;
-
 import com.google.gson.*;
-
 import javax.swing.*;
 
 /**
@@ -15,12 +12,7 @@ import javax.swing.*;
 
 public class Console {
     private static GsonBuilder builder = new GsonBuilder();
-//    builder.registerTypeAdapter(Humanoid.class, new CustomConverter());
-    private static Gson gsonHumanoid = builder.create();
-    private static Gson gson = new Gson(); //persons + items
-    private List<Predmet> things = new LinkedList<>();
-    private Scanner scanner = new Scanner(System.in);
-    private static String spliter = ",";
+    private static List<Predmet> baggage = new LinkedList<Predmet>();
     private static String helptxt = "remove_all {element}: удалить из коллекции все элементы, эквивалентные заданному\nremove {String key}: " +
             "удалить элемент из коллекции по его ключу\nshow: вывести в стандартный поток вывода все элементы коллекции в строковом представлении" +
             "\nadd_if_max {element}: добавить новый элемент в коллекцию, если его значение превышает значение наибольшего элемента этой коллекции" +
@@ -55,52 +47,24 @@ public class Console {
                 String a1[] = str.split("remove_all", 2);
                 String a11 = a1[1];
                 Integer hashCode;
-                Integer hash;
                 try{
-                    List<Predmet> baggage = new LinkedList<Predmet>();
+                    baggage.clear();
                     JsonElement jsonElement = new JsonParser().parse(a11);
                     JsonObject jsonObject = jsonElement.getAsJsonObject();
                     if (a11.contains("butilka")){
-                        JsonObject jsonObjectButilka = (JsonObject) jsonObject.get("butilka");
-                        String nameSTR = jsonObjectButilka.get("name").toString();
-                        String[] name1 =nameSTR.split("\"", 3);
-                        String name = name1[1];
-                        String valueSTR = jsonObjectButilka.get("value").toString();
-                        String[] value1 = valueSTR.split("\"", 3);
-                        Double value = Double.valueOf(value1[1]);
-                        baggage.add(new Predmet.Butilka(name, value));
+                        addBaggage(jsonObject, "butilka");
                     }
                     if (a11.contains("shlyapa")){
-                        JsonObject jsonObjectShlyapa = (JsonObject) jsonObject.get("shlyapa");
-                        String nameSTR = jsonObjectShlyapa.get("name").toString();
-                        String[] name1 =nameSTR.split("\"", 3);
-                        String name = name1[1];
-                        String valueSTR = jsonObjectShlyapa.get("value").toString();
-                        String[] value1 = valueSTR.split("\"", 3);
-                        Double value = Double.valueOf(value1[1]);
-                        baggage.add(new Predmet.Shlyapa(name, value));
+                        addBaggage(jsonObject, "shlyapa");
                     }
                     if (a11.contains("sumka")){
-                        JsonObject jsonObjectSumka = (JsonObject) jsonObject.get("sumka");
-                        String nameSTR = jsonObjectSumka.get("name").toString();
-                        String[] name1 =nameSTR.split("\"", 3);
-                        String name = name1[1];
-                        String valueSTR = jsonObjectSumka.get("value").toString();
-                        String[] value1 = valueSTR.split("\"", 3);
-                        Double value = Double.valueOf(value1[1]);
-                        baggage.add(new Sumka(name, value));
+                        addBaggage(jsonObject, "sumka");
                     }
                     if(!a11.contains("sumka") && !a11.contains("shlyapa") && !a11.contains("butilka") && a11.contains("null")) baggage = null;
                     List <Humanoid> keysToDelete = new LinkedList<Humanoid>();
                     hashCode = baggage.hashCode();
                     for (Humanoid key: map.keySet()) {
                         if (hashCode.equals(map.get(key).hashCode())) keysToDelete.add(key);
-//                        else hash = map.get(key).hashCode();
-//                        if ((map.get(key) == null) && (baggage == null)) keysToDelete.add(key);
-//                        if (map.get(key).equals(baggage)) {
-//                            keysToDelete.add(key);
-//                            System.out.println("Элемент удалён");
-//                        }
                     }
                     for (Humanoid key: keysToDelete){
                         map.remove(key);
@@ -156,7 +120,7 @@ public class Console {
                 String[] a4 = str.split("add_if_max", 2);
                 String a41 = a4[1];
                 try{
-                    List<Predmet> baggage4 = new LinkedList<Predmet>();
+                    baggage.clear();
                     JsonElement jsonElement = new JsonParser().parse(a41);
                     JsonObject jsonObject = jsonElement.getAsJsonObject();
                     JsonObject jsonObjectHumanoid = (JsonObject) jsonObject.get("human");
@@ -166,36 +130,14 @@ public class Console {
                     String palaceSTR = jsonObjectHumanoid.get("palace").toString();
                     String[] palace1 = palaceSTR.split("\"", 3);
                     Palace palace = Palace.valueOf(palace1[1]);
-                    JsonObject jsonObjectBagazh = (JsonObject) jsonObject.get("bagazh");
                     if (a41.contains("butilka")){
-                        JsonObject jsonObjectButilka = (JsonObject) jsonObjectBagazh.get("butilka");
-                        String nameSTR = jsonObjectButilka.get("name").toString();
-                        String[] name4 =nameSTR.split("\"", 3);
-                        String name = name4[1];
-                        String valueSTR = jsonObjectButilka.get("value").toString();
-                        String[] value1 = valueSTR.split("\"", 3);
-                        Double value = Double.valueOf(value1[1]);
-                        baggage4.add(new Predmet.Butilka(name, value));
+                        addBaggage(jsonObject, "butilka");
                     }
                     if (a41.contains("shlyapa")){
-                        JsonObject jsonObjectShlyapa = (JsonObject) jsonObjectBagazh.get("shlyapa");
-                        String nameSTR = jsonObjectShlyapa.get("name").toString();
-                        String[] name4 =nameSTR.split("\"", 3);
-                        String name = name4[1];
-                        String valueSTR = jsonObjectShlyapa.get("value").toString();
-                        String[] value1 = valueSTR.split("\"", 3);
-                        Double value = Double.valueOf(value1[1]);
-                        baggage4.add(new Predmet.Shlyapa(name, value));
+                        addBaggage(jsonObject, "shlyapa");
                     }
                     if (a41.contains("sumka")){
-                        JsonObject jsonObjectSumka = (JsonObject) jsonObjectBagazh.get("sumka");
-                        String nameSTR = jsonObjectSumka.get("name").toString();
-                        String[] name6 =nameSTR.split("\"", 3);
-                        String name = name1[1];
-                        String valueSTR = jsonObjectSumka.get("value").toString();
-                        String[] value1 = valueSTR.split("\"", 3);
-                        Double value = Double.valueOf(value1[1]);
-                        baggage4.add(new Sumka(name, value));
+                        addBaggage(jsonObject, "sumka");
                     }
                     if (map.size() > 0) {
                         maxHashCode = map.get(map.firstKey()).hashCode();
@@ -203,18 +145,18 @@ public class Console {
                             localHashCode = map.get(key).hashCode();
                             if (localHashCode > maxHashCode) maxHashCode = localHashCode;
                         }
-                        if (baggage4.hashCode() > maxHashCode) {
-                            map.put(new Humanoid(nameHumanoid, palace), baggage4);
+                        if (baggage.hashCode() > maxHashCode) {
+                            map.put(new Humanoid(nameHumanoid, palace), baggage);
                             System.out.println("Новый элемент был добавлен в коллецию: имя " + nameHumanoid + ", место " + palace);
-                            for (Predmet predmet : baggage4) {
+                            for (Predmet predmet : baggage) {
                                 System.out.println("Baggage:" + predmet.getClass() + ", name: " + predmet.name + ",  value: " + predmet.getValue() + ";");
                             }
                         }
                     }
                     else {
-                        map.put(new Humanoid(nameHumanoid, palace), baggage4);
+                        map.put(new Humanoid(nameHumanoid, palace), baggage);
                         System.out.println("Новый элемент был добавлен в коллецию: имя " + nameHumanoid + ", место " + palace);
-                        for (Predmet predmet : baggage4) {
+                        for (Predmet predmet : baggage) {
                             System.out.println("Baggage:" + predmet.getClass() + ", name: " + predmet.name + ",  value: " + predmet.getValue() + ";");
                         }
                     }
@@ -223,15 +165,14 @@ public class Console {
                 }
                 break;
             case 5:
-
-                System.out.println("Тип: " + map.getClass() + "; Кол-во элементов: " + map.size() + "; Время инициализации:" + Skazka.datePublic.toString() +
-                        "; Хэш-код: " + map.hashCode());
+                System.out.println("Тип: " + map.getClass() + "; Кол-во элементов: " + map.size() + "; Время инициализации:" +
+                        Skazka.datePublic.toString() + "; Хэш-код: " + map.hashCode());
                 break;
             case 6:
                 String a6[] = str.split("insert", 2);
                 String a61 = a6[1];
                 try{
-                    List<Predmet> baggage6 = new LinkedList<Predmet>();
+                    baggage.clear();
                     JsonElement jsonElement = new JsonParser().parse(a61);
                     JsonObject jsonObject = jsonElement.getAsJsonObject();
                     JsonObject jsonObjectHumanoid = (JsonObject) jsonObject.get("human");
@@ -241,40 +182,18 @@ public class Console {
                     String palaceSTR = jsonObjectHumanoid.get("palace").toString();
                     String[] palace1 = palaceSTR.split("\"", 3);
                     Palace palace = Palace.valueOf(palace1[1]);
-                    JsonObject jsonObjectBagazh = (JsonObject) jsonObject.get("bagazh");
                     if (a61.contains("butilka")){
-                        JsonObject jsonObjectButilka = (JsonObject) jsonObjectBagazh.get("butilka");
-                        String nameSTR = jsonObjectButilka.get("name").toString();
-                        String[] name6 =nameSTR.split("\"", 3);
-                        String name = name6[1];
-                        String valueSTR = jsonObjectButilka.get("value").toString();
-                        String[] value1 = valueSTR.split("\"", 3);
-                        Double value = Double.valueOf(value1[1]);
-                        baggage6.add(new Predmet.Butilka(name, value));
+                        addBaggage(jsonObject, "butilka");
                     }
                     if (a61.contains("shlyapa")){
-                        JsonObject jsonObjectShlyapa = (JsonObject) jsonObjectBagazh.get("shlyapa");
-                        String nameSTR = jsonObjectShlyapa.get("name").toString();
-                        String[] name6 =nameSTR.split("\"", 3);
-                        String name = name6[1];
-                        String valueSTR = jsonObjectShlyapa.get("value").toString();
-                        String[] value1 = valueSTR.split("\"", 3);
-                        Double value = Double.valueOf(value1[1]);
-                        baggage6.add(new Predmet.Shlyapa(name, value));
+                        addBaggage(jsonObject, "shlyapa");
                     }
                     if (a61.contains("sumka")){
-                        JsonObject jsonObjectSumka = (JsonObject) jsonObjectBagazh.get("sumka");
-                        String nameSTR = jsonObjectSumka.get("name").toString();
-                        String[] name6 =nameSTR.split("\"", 3);
-                        String name = name6[1];
-                        String valueSTR = jsonObjectSumka.get("value").toString();
-                        String[] value1 = valueSTR.split("\"", 3);
-                        Double value = Double.valueOf(value1[1]);
-                        baggage6.add(new Sumka(name, value));
+                        addBaggage(jsonObject, "sumka");
                     }
-                    map.put(new Humanoid(nameHumanoid, palace), baggage6);
+                    map.put(new Humanoid(nameHumanoid, palace), baggage);
                     System.out.println("Новый элемент был добавлен в коллецию: имя " + nameHumanoid + ", место " + palace);
-                    for (Predmet predmet : baggage6){
+                    for (Predmet predmet : baggage){
                         System.out.println("Baggage:" + predmet.getClass() + ", name: " + predmet.name + ",  value: " + predmet.getValue() + ";");
                     }
                     }catch (Exception e){
@@ -282,61 +201,37 @@ public class Console {
                         e.printStackTrace();
                     }
 
-
                 break;
             case 7:
                 String[] a7 = str.split("remove_lower");
                 String a71 = a7[1];
-            Integer hashCode7;
-            Integer hash7;
             int count = 0;
             try{
-                List<Predmet> baggage7 = new LinkedList<Predmet>();
+                baggage.clear();
                 JsonElement jsonElement7 = new JsonParser().parse(a71);
-                JsonObject jsonObject7 = jsonElement7.getAsJsonObject();
-                if (a71.contains("butilka")){
-                    JsonObject jsonObjectButilka = (JsonObject) jsonObject7.get("butilka");
-                    String nameSTR = jsonObjectButilka.get("name").toString();
-                    String[] name7 =nameSTR.split("\"", 3);
-                    String name = name7[1];
-                    String valueSTR = jsonObjectButilka.get("value").toString();
-                    String[] value1 = valueSTR.split("\"", 3);
-                    Double value = Double.valueOf(value1[1]);
-                    baggage7.add(new Predmet.Butilka(name, value));
+                JsonObject jsonObject = jsonElement7.getAsJsonObject();
+                if (a71.contains("butilka")) {
+                    addBaggage(jsonObject, "butilka");
                 }
-                if (a71.contains("shlyapa")){
-                    JsonObject jsonObjectShlyapa = (JsonObject) jsonObject7.get("shlyapa");
-                    String nameSTR = jsonObjectShlyapa.get("name").toString();
-                    String[] name7 =nameSTR.split("\"", 3);
-                    String name = name7[1];
-                    String valueSTR = jsonObjectShlyapa.get("value").toString();
-                    String[] value1 = valueSTR.split("\"", 3);
-                    Double value = Double.valueOf(value1[1]);
-                    baggage7.add(new Predmet.Shlyapa(name, value));
+                if (a71.contains("shlyapa")) {
+                    addBaggage(jsonObject, "shlyapa");
                 }
-                if (a71.contains("sumka")){
-                    JsonObject jsonObjectSumka = (JsonObject) jsonObject7.get("sumka");
-                    String nameSTR = jsonObjectSumka.get("name").toString();
-                    String[] name7 =nameSTR.split("\"", 3);
-                    String name = name7[1];
-                    String valueSTR = jsonObjectSumka.get("value").toString();
-                    String[] value1 = valueSTR.split("\"", 3);
-                    Double value = Double.valueOf(value1[1]);
-                    baggage7.add(new Sumka(name, value));
+                if (a71.contains("sumka")) {
+                    addBaggage(jsonObject, "");
                 }
                 List <Humanoid> keysToDelete = new LinkedList<Humanoid>();
-                hashCode = baggage7.hashCode();
+                hashCode = baggage.hashCode();
                 System.out.println("Input baggage hashcode is: " + hashCode);
                 for (Humanoid key: map.keySet()) {
                     if (hashCode > (map.get(key).hashCode())) keysToDelete.add(key);
                 }
-                for (Humanoid key: keysToDelete){
+                for (Humanoid key: keysToDelete) {
                     map.remove(key);
                     count++;
                 }
                 System.out.println("Удалено " + count + " элементов");
 
-            }catch (Exception e){
+            }catch (Exception e) {
                 System.out.println("Попробуйте ещё раз");
                 e.printStackTrace();
             }
@@ -360,20 +255,26 @@ public class Console {
                 System.out.println("АААА, сложна, введите команду ещё раз");
                 break;
         }
-
     }
 
-    private static String element(String str) {
-        String foo[] = str.split("\\{");
-        String bar[] = foo[1].split("}");
-        foo = bar[0].split(":");
-        bar = foo[1].split("\"");
-        System.out.println(bar[1]);
-        String baz = bar[0];
-        return baz;
-    }
-
-    public static void setGson(Gson gson) {
-        Console.gson = gson;
+    private static void addBaggage(JsonObject jsonObject, String s) {
+        JsonObject jsonObjectBaggage = (JsonObject) jsonObject.get(s);
+        String nameSTR = jsonObjectBaggage.get("name").toString();
+        String[] names =nameSTR.split("\"", 3);
+        String name = names[1];
+        String valueSTR = jsonObject.get("value").toString();
+        String[] value1 = valueSTR.split("\"", 3);
+        Double value = Double.valueOf(value1[1]);
+        switch (s) {
+            case "butilka":
+                baggage.add(new Predmet.Butilka(name, value));
+                break;
+            case "shlyapa":
+                baggage.add(new Predmet.Shlyapa(name, value));
+                break;
+            case "sumka" :
+                baggage.add(new Sumka(name, value));
+                break;
+        }
     }
 }
