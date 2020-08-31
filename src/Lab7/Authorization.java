49ed99f0632e003cf245379d2bf73995cab35561
Antipin Arsentii <aantipin@medlinesoft.ru>
2020-09-01 02:37:47 +0300

    Exctract sign in to method and add sql config. Working on authorization

--- src/Lab7/Authorization.java
+++ src/Lab7/Authorization.java
@@ -1,5 +1,6 @@
 package Lab7;
 
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +13,13 @@ public class Authorization {
 
 //    think about add singleton https://habr.com/ru/post/129494/
 
+
     private static Map<User, Integer> authenticator = new HashMap<>();
+    /*
+    signIn return 0 if user.name not found
+    return -1 if wrong password
+    if user.name found and password hashes are equal return user.Id
+     */
     public static int signIn(User user){
         return authenticator.getOrDefault(user, -1);
 //        with db use
@@ -20,7 +27,19 @@ public class Authorization {
 //        where name = 'name';
 
     }
-    public static int addUser (User user){
-        return authenticator.put(user, authenticator.size()+1) == null ? authenticator.get(user) : -1;
+    public static void addUser (User user) throws ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        try (Connection connection = DriverManager.getConnection(DBconfigs.url, DBconfigs.dbUser, DBconfigs.dbPassword)){
+//            Statement statement = connection.createStatement();
+            String sql = "INSERT INTO user (name, password) Values (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, user.name);
+            preparedStatement.setString(2, user.password);
+            int rows = preparedStatement.executeUpdate();
+            System.out.printf("%d rows added", rows);
+    } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
+//    return authenticator.put(user, authenticator.size()+1) == null ? authenticator.get(user) : -1;
